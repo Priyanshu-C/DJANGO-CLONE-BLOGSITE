8ffa44ed78cb5755cd44dc38be5e9f55856f46b7
Priyanshu-C <63398163+Priyanshu-C@users.noreply.github.com>
2020-09-23 00:14:40 +0530

    All web-pages set

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     path('home/',views.PostListView.as_view,name='post_list'),
     path('about/',views.AboutView.as_view,name='about'),
     path('post/<int:pk>',views.PostDetailView.as_view,name='post_detail'),
-    path('post/new/',views.CreatePostView.as_view,name='post_create'),
+    path('post/new/',views.CreatePostView.as_view,name='post_new'),
     path('post/<int:pk>/edit/',views.PostUpdateView.as_view,name='post_edit'),
     path('post/<int:pk>/delete/',views.PostDetailView.as_view,name='post_delete'),
     path('post/drafts/',views.DraftListView,name='post_draft_list'),
+    path('post/<int:pk>/comment/',views.add_comment_to_post,name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/',views.comment_approve,name='comment_approve'),
+    path('comment/<int:pk>/remove/',views.comment_remove,name='comment_remove'),
+    path('post/<int:pk>/publish/',views.post_publish,name='post_publish'),
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blogs.urls')),
+    path('accounts/login/',views.login,name='login'),
+    path('accounts/logout/',views.logout,name='login',kwargs={'next_page=':'/'})
 ]
